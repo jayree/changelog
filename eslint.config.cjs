47b3f749bc914481243bb8b829ee4dd194b5cc05
jayree <jayree.git@icloud.com>
2025-10-31 12:37:58 +0100

    chore: fix

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,14 @@
 const { FlatCompat } = require('@eslint/eslintrc');
 const js = require('@eslint/js');
 
+const unwrapEsmPlugin = (plugin) => {
+  if (plugin && typeof plugin === 'object' && 'default' in plugin && plugin.default) {
+    return plugin.default;
+  }
+
+  return plugin;
+};
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   resolvePluginsRelativeTo: __dirname,
@@ -9,26 +17,39 @@ const compat = new FlatCompat({
 });
 
 module.exports = [
-  ...compat.config({
-    extends: ['eslint-config-salesforce-typescript', 'plugin:prettier/recommended'],
-    plugins: ['eslint-plugin-header'],
-    ignorePatterns: ['**/*.d.ts', '**/*.cjs'],
-    rules: {
-      'header/header': [
-        2,
-        'block',
-        [
-          '',
-          {
-            pattern: ' \\* Copyright \\(c\\) \\d{4}, jayree',
-            template: ' * Copyright (c) 2023, jayree',
-          },
-          ' * All rights reserved.',
-          ' * Licensed under the BSD 3-Clause license.',
-          ' * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
-          ' ',
+  {
+    ignores: ['**/*.d.ts', '**/*.cjs'],
+  },
+  ...compat
+    .config({
+      extends: ['eslint-config-salesforce-typescript', 'plugin:prettier/recommended'],
+      plugins: ['eslint-plugin-header'],
+      rules: {
+        'header/header': [
+          2,
+          'block',
+          [
+            '',
+            {
+              pattern: ' \\* Copyright \\(c\\) \\d{4}, jayree',
+              template: ' * Copyright (c) 2023, jayree',
+            },
+            ' * All rights reserved.',
+            ' * Licensed under the BSD 3-Clause license.',
+            ' * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause',
+            ' ',
+          ],
         ],
-      ],
-    },
-  }),
+      },
+    })
+    .map((config) =>
+      config.plugins
+        ? {
+            ...config,
+            plugins: Object.fromEntries(
+              Object.entries(config.plugins).map(([name, plugin]) => [name, unwrapEsmPlugin(plugin)]),
+            ),
+          }
+        : config,
+    ),
 ];
