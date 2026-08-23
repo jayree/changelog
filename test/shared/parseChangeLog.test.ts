60458fb1dadd0fce1a77abf320fdb22bcb699b73
jayree <jayree.git@icloud.com>
2026-08-23 03:40:31 +0200

    chore: ts7

--- test/shared/parseChangeLog.test.ts
+++ test/shared/parseChangeLog.test.ts
@@ -1,8 +1,17 @@
 /*
- * Copyright (c) 2023, jayree
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ * Copyright 2026, jayree
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 // original from https://github.com/salesforcecli/plugin-info/blob/main/test/shared/parseReleaseNotes.test.ts
 import { dirname } from 'node:path';
@@ -20,7 +29,6 @@ const __filename = fileURLToPath(import.meta.url);
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(__filename);
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 chaiUse(SinonChai);
 
 describe('parseReleaseNotes tests', () => {
@@ -41,7 +49,6 @@ describe('parseReleaseNotes tests', () => {
   it('calls lexer with raw release notes', () => {
     parseChangeLog(notes, '7.121.8', '7.120.0');
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     expect(lexerSpy.called).to.be.true;
     expect(lexerSpy.args[0][0]).to.deep.equal(notes);
   });
@@ -62,7 +69,6 @@ describe('parseReleaseNotes tests', () => {
     try {
       parseChangeLog(notes, '1.2.3', '7.120.0');
     } catch (err) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       expect((err as Error).message).to.equal(`Didn't find version '1.2.3'. View release notes online at: ${baseUrl}`);
     }
   });
@@ -79,7 +85,6 @@ describe('parseReleaseNotes tests', () => {
       // Won't find partial version (3.3.1 is part of 13.3.1)
       parseChangeLog(notes, '3.3.1', '7.120.0');
     } catch (err) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       expect((err as Error).message).to.equal(`Didn't find version '3.3.1'. View release notes online at: ${baseUrl}`);
     }
   });
